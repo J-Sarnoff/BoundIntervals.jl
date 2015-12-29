18015dae3e20f7739fe6d8f248c18733b0578d45
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 03:54:55 -0500

    Update mul.jl

--- src/arith/round/mul.jl
+++ src/arith/round/mul.jl
@@ -1,1 +1,66 @@
+#=
+    There are nine general cases for processing a*b
+                   b <= 0    b <0< b    b >= 0
+       a <=  0    LteLte      LteZer    LteGte
+       a <0< a    ZerLte      ZerZer    ZerGte
+       a >=  0    GteLte      GteZer    GteGte
+=#
 
+function (*){B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b::Ivl{B2})
+    z = 0.0
+    if     a.hi <= z
+       if     b.hi <= z
+           mulLteLte(a,b)
+       elseif b.lo >= z
+           mulLteGte(a,b)
+       else   # b straddles 0
+           mulLteZer(a,b)
+       end
+    elseif a.lo >= z
+       if     b.hi <= z
+           mulGteLte(a,b)
+       elseif b.lo >= z
+           mulGteGte(a,b)
+       else   # b straddles 0
+           mulGteZer(a,b)
+       end
+    else   # a straddles 0
+       if     b.hi <= z
+           mulZerLte(a,b)
+       elseif b.lo >= z
+           mulZerGte(a,b)
+       else   # b straddles 0
+           mulZerZer(a,b)
+       end
+    end
+end
+
+for (fn,loa,lob,hia,hib) in [ (:mulLteLte, :(a.hi), :(b.hi), :(a.lo), :(b.lo)),
+                              (:mulLteGte, :(a.lo), :(b.hi), :(a.hi), :(b.lo)),
+                              (:mulLteZer, :(a.lo), :(b.hi), :(a.lo), :(b.lo)),
+                              (:mulGteLte, :(a.hi), :(b.lo), :(a.lo), :(b.hi)),
+                              (:mulGteGte, :(a.lo), :(b.lo), :(a.hi), :(b.hi)),
+                              (:mulGteZer, :(a.hi), :(b.lo), :(a.hi), :(b.hi)),
+                              (:mulZerLte, :(a.hi), :(b.lo), :(a.lo), :(b.lo)),
+                              (:mulZerGte, :(a.lo), :(b.hi), :(a.hi), :(b.hi)),
+                            ]
+  @eval begin
+    function ($fn){B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b::Ivl{B2})
+        aLoIsOpen, aHiIsOpen = bounds(B1)
+        bLoIsOpen, bHiIsOpen = bounds(B2)
+        newBound = bound( (aLoIsOpen|bLoIsOpen), (aHiIsOpen|bHiIsOpen) )
+    
+        T = promote_type(promote_type(typeof(a.lo),typeof(b.lo)),promote_type(typeof(a.hi),typeof(b.hi)))
+        lo=hi=zero(T)
+    
+        with_rounding(T,RoundDown) do
+            lo = (*)(($loa), ($lob))
+        end
+        with_rounding(T,RoundUp) do
+            hi = (*)(($hia), ($hib))
+        end    
+
+        Ivl{newBound}(lo, hi)
+    end
+  end
+end
