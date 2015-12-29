57c72a6d25b643157a34f929494da754a8879116
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 02:20:41 -0500

    Update sub.jl

--- src/arith/plain/sub.jl
+++ src/arith/plain/sub.jl
@@ -1,1 +1,17 @@
+@inline function (-){B<:AkoBound}(a::Ivl{B})
+   Ivl{negate(B)}(-a.hi, -a.lo)
+end
+
+
+function (-){B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b::Ivl{B2})
+    aLoIsOpen, aHiIsOpen = bounds(B1)
+    bLoIsOpen, bHiIsOpen = bounds(B2)
+
+    newBound = bound( (aLoIsOpen|bLoIsOpen), (aHiIsOpen|bHiIsOpen) )
+
+    lo = (-)(a.lo, b.hi) #  RoundDown
+    hi = (-)(a.hi, b.lo) #  RoundUp
+
+    Ivl{newBound}(lo, hi)
+end
 
