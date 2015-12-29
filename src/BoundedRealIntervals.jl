5a4955273cba302bb3175af57c0236eb2107e8b4
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 01:59:25 -0500

    Update BoundedRealIntervals.jl

--- src/BoundedRealIntervals.jl
+++ src/BoundedRealIntervals.jl
@@ -8,6 +8,19 @@ import Base: convert, promote_type,
 include("type/abstract.jl")
 include("type/concrete.jl")
 include("type/enfold.jl")
+include("type/io.jl")
+
+if isdefined(Main,:UseRounding) && (Main.UseRounding==true)
+include("arith/round/add.jl")
+include("arith/round/sub.jl")
+include("arith/round/mul.jl")
+include("arith/round/div.jl")
+else
+include("arith/plain/add.jl")
+include("arith/plain/sub.jl")
+include("arith/plain/mul.jl")
+include("arith/plain/div.jl")
+end # UseRounding
 
 
 end # module
