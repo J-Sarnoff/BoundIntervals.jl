696bbbeacf8208e482712e8ab8923ffb9da62a88
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 04:01:19 -0500

    Update sqrt.jl

--- src/arith/round/sqrt.jl
+++ src/arith/round/sqrt.jl
@@ -1,1 +1,13 @@
-
+function sqrt{B<:AkoBound}(a::Ivl{B})
+    T = promote_type(typeof(a.lo),typeof(a.hi))
+    lo=hi=zero(T)
+    
+    with_rounding(T,RoundDown) do
+        lo = sqrt(a.lo)
+    end
+    with_rounding(T,RoundDown) do
+        hi = sqrt(a.hi)
+    end
+    
+    Ivl{B}(lo, hi)
+end
