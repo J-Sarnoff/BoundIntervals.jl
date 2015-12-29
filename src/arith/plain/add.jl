afb5e046c8ff0f534912e4bf00c0f82a1e768702
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 02:18:17 -0500

    Update add.jl

--- src/arith/plain/add.jl
+++ src/arith/plain/add.jl
@@ -1,1 +1,12 @@
+function (+){B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b::Ivl{B2})
+    aLoIsOpen, aHiIsOpen = bounds(B1)
+    bLoIsOpen, bHiIsOpen = bounds(B2)
+
+    newBound = bound( (aLoIsOpen|bLoIsOpen), (aHiIsOpen|bHiIsOpen) )
+
+    lo = (+)(a.lo, b.lo) #  RoundDown
+    hi = (+)(a.hi, b.hi) #  RoundUp
+
+    Ivl{newBound}(lo, hi)
+end
 
