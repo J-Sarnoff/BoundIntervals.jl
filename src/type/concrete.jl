ffd01a1851c46f33e72d06b2adcbf41f88e503d2
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 00:01:29 -0500

    Update concrete.jl

--- src/type/concrete.jl
+++ src/type/concrete.jl
@@ -8,4 +8,19 @@ immutable Ivl{B<:AkoBound}  <: XtdRealInterval
     hi::Real
 end
 
+for (op,T) in [(:ClCl,:CC), (:ClOp,:CO), (:OpCl,:OC), (:OpOp, :OO)]
+  @eval begin
+    function ($op)(lo::Real, hi::Real)
+        low,hig= promote(lo,hi)
+        if (low > hig)
+           Ivl{$T}(hi,lo)
+        else
+           Ivl{$T}(lo,hi)
+        end
+    end
+    function ($op){R<:Real}(lo::R, hi::R) = Ivl{$T}(minmax(lo,hi)...)
+    function ($op)(x::Real) = Ivl{$T}(x,x)
+  end
+end  
+Xact(x::Real) = Ivl{XA}(x,x)
 
