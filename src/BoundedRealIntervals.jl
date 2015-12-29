98e9d932db1c7578540fe701781bd9e6cb931bf9
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-28 23:20:00 -0500

    Update BoundedRealIntervals.jl

--- src/BoundedRealIntervals.jl
+++ src/BoundedRealIntervals.jl
@@ -1,6 +1,10 @@
 module BoundedRealIntervals
 
-
+import Base: convert, promote_type,
+    string, hash, show, showcompact,
+    sign, changesign, flipsign, abs
+    
+    
 include("type/abstract.jl")
 include("type/concrete.jl")
 
