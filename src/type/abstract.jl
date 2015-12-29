8ee88f38eda011d3df7005565b11acef8818830b
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-28 23:17:55 -0500

    Update abstract.jl

--- src/type/abstract.jl
+++ src/type/abstract.jl
@@ -6,5 +6,10 @@ abstract CC  <: AkoBound # closed-closed
 abstract CO  <: AkoBound # closed-opened
 abstract OC  <: AkoBound # opened-closed
 abstract OO  <: AkoBound # opened-opened
-abstract EX  <: CC       # closed-closed where lo==hi, 'exact'
+abstract XT  <: CC       # closed-closed where lo==hi, 'exact'
 
+string(::Type{CC}) = "ClCl"
+string(::Type{CO}) = "ClOp"
+string(::Type{OC}) = "OpCl"
+string(::Type{OO}) = "OpOp"
+string(::Type{XT}) = "Xact"
