a27a95c5995cd0395b5503ec286ec824271eeeff
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 03:53:24 -0500

    Update sub.jl

--- src/arith/round/sub.jl
+++ src/arith/round/sub.jl
@@ -1,1 +1,24 @@
+@inline function (-){B<:AkoBound}(a::Ivl{B})
+   Ivl{swapBound[B]}(-a.hi, -a.lo)
+end
+
+
+function (-){B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b::Ivl{B2})
+    aLoIsOpen, aHiIsOpen = bounds(B1)
+    bLoIsOpen, bHiIsOpen = bounds(B2)
+
+    newBound = bound( (aLoIsOpen|bLoIsOpen), (aHiIsOpen|bHiIsOpen) )
+    
+    T = promote_type(promote_type(typeof(a.lo),typeof(b.lo)),promote_type(typeof(a.hi),typeof(b.hi)))
+    lo=hi=zero(T)
+    
+    with_rounding(T,RoundDown) do
+        lo = (-)(a.lo, b.hi)
+    end
+    with_rounding(T,RoundUp) do
+        hi = (-)(a.hi, b.lo)
+    end
+
+    Ivl{newBound}(lo, hi)
+end
 
