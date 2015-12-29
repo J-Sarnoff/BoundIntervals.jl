5f21d1d625df6c565fdc51cd09f0509ee256af3b
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 04:02:03 -0500

    Update sqr.jl

--- src/arith/round/sqr.jl
+++ src/arith/round/sqr.jl
@@ -1,1 +1,13 @@
-
+function sqr{B<:AkoBound}(a::Ivl{B})
+    T = promote_type(typeof(a.lo),typeof(a.hi))
+    lo=hi=zero(T)
+    
+    with_rounding(T,RoundDown) do
+        lo = a.lo * a.lo
+    end
+    with_rounding(T,RoundUp) do
+        hi = a.hi * a.hi
+    end
+    
+    Ivl{B}(lo, hi)
+end
