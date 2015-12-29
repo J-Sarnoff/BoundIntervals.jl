e078cb8ca0f6a9e8da69dfb96249f0f117d3950c
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 01:01:25 -0500

    Update enfold.jl

--- src/type/enfold.jl
+++ src/type/enfold.jl
@@ -6,3 +6,10 @@ ClCl( x::Ivl{ClOp} ) = Ivl{ClCl}(x.lo,x.hi)
 ClCl( x::Ivl{OpCl} ) = Ivl{ClCl}(x.lo,x.hi)
 ClCl( x::Ivl{OpOp} ) = Ivl{ClCl}(x.lo,x.hi)
 
+
+OpOp( x::Ivl{Xact} ) = Ivl{OpOp}(prevfloat(x.lo),nextfloat(x.hi))
+OpOp( x::Ivl{ClCl} ) = Ivl{OpOp}(prevfloat(x.lo),nextfloat(x.hi))
+OpOp( x::Ivl{ClOp} ) = Ivl{OpOp}(prevfloat(x.lo),x.hi)
+OpOp( x::Ivl{OpCl} ) = Ivl{OpOp}(x.lo,nextfloat(x.hi))
+OpOp( x::Ivl{OpOp} ) = x
+
