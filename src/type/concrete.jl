f98d6374d54fd26e64cdf33cc288e503574f290f
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-28 23:50:37 -0500

    Update concrete.jl

--- src/type/concrete.jl
+++ src/type/concrete.jl
@@ -3,7 +3,7 @@
 # allow lo, hi to differ in type (subtypes of Real)
 # R <-- promote_type(typeof(lo),typeof(hi))
 
-immutable Ivl{B<:AkoBound}  <: RealInterval
+immutable Ivl{B<:AkoBound}  <: XtdRealInterval
     lo::Real                        
     hi::Real
 end
