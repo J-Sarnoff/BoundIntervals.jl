cb792c20a00d131102f47c64a5f11843f1c6127e
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-29 01:34:07 -0500

    Update enfold.jl

--- src/type/enfold.jl
+++ src/type/enfold.jl
@@ -1,32 +1,32 @@
 # applicative conversion
 
-Xact( x::Ivl{Xact} ) = x
-Xact( x::Ivl{ClCl} ) = Ivl{XA}(x.lo/2+x.hi/2)
-Xaxt( x::Ivl{ClOp} ) = Ivl{XA}(x.lo/2+x.hi/2)
-Xact( x::Ivl{OpCl} ) = Ivl{XA}(x.lo/2+x.hi/2)
-Xact( x::Ivl{OpOp} ) = Ivl{XA}(x.lo/2+x.hi/2)
+Xact( x::Ivl{XA} ) = x
+Xact( x::Ivl{CC} ) = Ivl{XA}(x.lo/2+x.hi/2)
+Xaxt( x::Ivl{CO} ) = Ivl{XA}(x.lo/2+x.hi/2)
+Xact( x::Ivl{OC} ) = Ivl{XA}(x.lo/2+x.hi/2)
+Xact( x::Ivl{OO} ) = Ivl{XA}(x.lo/2+x.hi/2)
 
-ClCl( x::Ivl{Xact} ) = Ivl{CC}(x.lo,x.hi)
-ClCl( x::Ivl{ClCl} ) = x
-ClCl( x::Ivl{ClOp} ) = Ivl{CC}(x.lo,x.hi)
-ClCl( x::Ivl{OpCl} ) = Ivl{CC}(x.lo,x.hi)
-ClCl( x::Ivl{OpOp} ) = Ivl{CC}(x.lo,x.hi)
+ClCl( x::Ivl{XA} ) = Ivl{CC}(x.lo,x.hi)
+ClCl( x::Ivl{CC} ) = x
+ClCl( x::Ivl{CO} ) = Ivl{CC}(x.lo,x.hi)
+ClCl( x::Ivl{OC} ) = Ivl{CC}(x.lo,x.hi)
+ClCl( x::Ivl{OO} ) = Ivl{CC}(x.lo,x.hi)
 
-ClOp( x::Ivl{Xact} ) = Ivl{CO}(x.lo,nextfloat(x.hi))
-ClOp( x::Ivl{ClCl} ) = Ivl{CO}(x.lo,nextfloat(x.hi))
-ClOp( x::Ivl{ClOp} ) = x
-ClOp( x::Ivl{OpCl} ) = Ivl{CO}(x.lo,nextfloat(x.hi))
-ClOp( x::Ivl{OpOp} ) = Ivl{CO}(x.lo,x.hi)
+ClOp( x::Ivl{XA} ) = Ivl{CO}(x.lo,nextfloat(x.hi))
+ClOp( x::Ivl{CC} ) = Ivl{CO}(x.lo,nextfloat(x.hi))
+ClOp( x::Ivl{CO} ) = x
+ClOp( x::Ivl{OC} ) = Ivl{CO}(x.lo,nextfloat(x.hi))
+ClOp( x::Ivl{OO} ) = Ivl{CO}(x.lo,x.hi)
 
-OpCl( x::Ivl{Xact} ) = Ivl{OC}(prevfloat(x.lo),x.hi)
-OpCl( x::Ivl{ClCl} ) = Ivl{OC}(prevfloat(x.lo),x.hi)
-OpCl( x::Ivl{ClOp} ) = Ivl{OC}(prevfloat(x.lo),x.hi)
-OpCl( x::Ivl{OpCl} ) = x
-OpCl( x::Ivl{OpOp} ) = Ivl{OC}(x.lo,nextfloat(x.hi))
+OpCl( x::Ivl{XA} ) = Ivl{OC}(prevfloat(x.lo),x.hi)
+OpCl( x::Ivl{CC} ) = Ivl{OC}(prevfloat(x.lo),x.hi)
+OpCl( x::Ivl{CO} ) = Ivl{OC}(prevfloat(x.lo),x.hi)
+OpCl( x::Ivl{OC} ) = x
+OpCl( x::Ivl{OO} ) = Ivl{OC}(x.lo,nextfloat(x.hi))
 
-OpOp( x::Ivl{Xact} ) = Ivl{OO}(prevfloat(x.lo),nextfloat(x.hi))
-OpOp( x::Ivl{ClCl} ) = Ivl{OO}(prevfloat(x.lo),nextfloat(x.hi))
-OpOp( x::Ivl{ClOp} ) = Ivl{OO}(prevfloat(x.lo),x.hi)
-OpOp( x::Ivl{OpCl} ) = Ivl{OO}(x.lo,nextfloat(x.hi))
-OpOp( x::Ivl{OpOp} ) = x
+OpOp( x::Ivl{XA} ) = Ivl{OO}(prevfloat(x.lo),nextfloat(x.hi))
+OpOp( x::Ivl{CC} ) = Ivl{OO}(prevfloat(x.lo),nextfloat(x.hi))
+OpOp( x::Ivl{CO} ) = Ivl{OO}(prevfloat(x.lo),x.hi)
+OpOp( x::Ivl{OC} ) = Ivl{OO}(x.lo,nextfloat(x.hi))
+OpOp( x::Ivl{OO} ) = x
 
