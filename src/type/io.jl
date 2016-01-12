97bcc56943cfe10f7813aa36171dc1ac780e4380
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2016-01-12 09:28:53 -0500

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -1,5 +1,5 @@
 function show{B<:AkoBound}(io::IO, x::Ivl{B})
-    if B!=XA
+    if x.lo != x.hi
        s = string(string(B),"(",x.lo,", ",x.hi,")")
     else
        s = string(string(B),"(",x.lo,")")
