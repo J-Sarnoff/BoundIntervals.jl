7c8a5d350bfe8e718a8224bd064349783d5e3a52
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2016-01-11 19:14:06 -0500

    Update mul.jl

--- src/arith/round/mul.jl
+++ src/arith/round/mul.jl
@@ -66,7 +66,7 @@ for (fn,loa,lob,hia,hib) in [ (:mulLteLte, :(a.hi), :(b.hi), :(a.lo), :(b.lo)),
   end
 end
 
-function mulZerZer{B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b:Ivl{B2})
+function mulZerZer{B1<:AkoBound,B2<:AkoBound}(a::Ivl{B1}, b::Ivl{B2})
         aLoIsOpen, aHiIsOpen = bounds(B1)
         bLoIsOpen, bHiIsOpen = bounds(B2)
         newBound = bound( (aLoIsOpen|bLoIsOpen), (aHiIsOpen|bHiIsOpen) )
